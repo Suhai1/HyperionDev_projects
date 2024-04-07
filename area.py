b24cb699bd9ea080500e7f3793b93b798a47c948
Suhai1 <134132664+Suhai1@users.noreply.github.com>
2024-04-07 06:13:51 +0200

    Add files via upload

---
+++ area.py
@@ -0,0 +1,11 @@
+def calculate_rectangle_a(x, y):
+    return x * y
+
+def main():
+    x = float(input("Enter the x of the rectangle: "))
+    y = float(input("Enter the y of the rectangle: "))
+    a = calculate_rectangle_a(x, y)
+    print("The a of the rectangle is:", a)
+
+if __name__ == "__main__":
+    main()
